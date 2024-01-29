b5b71817af59a13c050f8bcd3cf9dd6858d8047f
Jafin Jahfar <jafinjahfar@gmail.com>
2024-01-29 07:08:21 +0530

    feat(app/common): created a common logger module

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,15 @@
+import { IsDate, IsNotEmpty } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsNotEmpty()
+  @IsDate()
   startDate: Date;
+
+  @IsNotEmpty()
+  @IsDate()
   endDate: Date;
+
+  @IsNotEmpty()
   placeId: string;
   invoiceId: string;
 }

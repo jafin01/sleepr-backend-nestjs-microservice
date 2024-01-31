903ea9326fb0cfded947364c9d526fd45e997555
Jafin Jahfar <jafinjahfar@gmail.com>
2024-02-01 04:40:24 +0530

    refactor(app/common/config): updated configurations to include individual env vars for each service
    added environment to docker-compose.yaml to look for in each service
    updated the config module to include individual configuration for each microservice
    refactored the return object in config service to occupy all the databases the microservice use
    added the service name in env on boot up to differentiate each service

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { ConfigService } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.useLogger(app.get(Logger));
+  const PORT = app.get(ConfigService).get('auth').port;
+  await app.listen(PORT, () => {
+    console.log(
+      `Server is active and listening on port ${PORT} at ${
+        app.get(ConfigService).get('auth').port
+      } as of ${new Date().toLocaleString()}`,
+    );
+  });
+}
+bootstrap();
--- libs/common/src/config/config.interface.ts
+++ libs/common/src/config/config.interface.ts
@@ -1,17 +1,52 @@
+<<<<<<< Updated upstream
 export interface ApiConfigProps {
   apiUrl: string;
   httpTimeout: number;
 }
 
+=======
+>>>>>>> Stashed changes
 export interface MongodbConfigProps {
-  connectionString: string;
-  databaseName: string;
+  uri: string;
+  name: string;
 }
 
+<<<<<<< Updated upstream
 export interface ConfigProps {
   port: number;
   api: ApiConfigProps;
   mongodb: {
     database: MongodbConfigProps;
   };
+=======
+export interface DatabaseProps {
+  mongodb?: MongodbConfigProps;
+}
+
+export interface CommonDatabaseProps {
+  databases: DatabaseProps;
+}
+
+export interface ReservationProps {
+  port: number;
+  api: string;
+  httpTimeout: number;
+  databases: DatabaseProps;
+}
+export interface AuthProps {
+  port: number;
+  api: string;
+  httpTimeout: number;
+  jwt: {
+    secret: string;
+    expiresIn: string;
+  };
+  databases: DatabaseProps;
+}
+
+export interface ConfigProps {
+  databases?: DatabaseProps;
+  reservations?: ReservationProps;
+  auth?: AuthProps;
+>>>>>>> Stashed changes
 }

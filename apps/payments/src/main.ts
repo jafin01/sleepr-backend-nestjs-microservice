32d1995b768d83af6ef75d9f85b7a93ce5002cc6
Jafin Jahfar <jafinjahfar@gmail.com>
2024-02-04 09:25:34 +0530

    feat(app/payments): created payments microservice app

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('payments').port,
+    },
+  });
+  await app.listen(3000);
+}
+bootstrap();

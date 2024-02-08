3613e07c99c321a0f8aad62b7e8ae1cb2a7932da
Jafin Jahfar <jafinjahfar@gmail.com>
2024-02-08 05:46:14 +0530

    build(ci/cd, dockerfile): created cloudbuild.yaml file for ci/cd, optimised production images

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { CurrentUser } from '@app/common';
-import { UserDocument } from './models/user.schema';
+import { UserDocument } from '@app/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('users')
--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from '@app/common';
-import { UserDocument, userSchema } from './models/user.schema';
+import { UserDocument, userSchema } from '@app/common';
 import { UserRepository } from './users.repository';
 
 @Module({
